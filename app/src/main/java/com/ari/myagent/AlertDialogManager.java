1c5b1e7c457c7459a9370cdd7a208496ca2a65e3
AlfinaFebiani <alfinafebiani03@gmail.com>
2022-06-13 22:20:13 +0700

    My Agent

---
+++ app/src/main/java/com/ari/myagent/AlertDialogManager.java
@@ -0,0 +1,4 @@
+package com.ari.myagent;
+
+public class AlertDialogManager {
+}
---
+++ app/src/main/java/com/ari/myagent/SessionManager.java
@@ -0,0 +1,15 @@
+package com.ari.myagent;
+
+import android.content.Context;
+
+public class SessionManager {
+    public SessionManager(Object applicationContext) {
+
+    }
+
+    public void checkLogin() {
+    }
+
+    public void logoutUser() {
+    }
+}
